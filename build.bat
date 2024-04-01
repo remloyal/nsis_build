9af930470e143f5559bb93fefd1364ad780b7833
remloyal <1182387960@qq.com>
2024-04-01 11:38:48 +0800

    添加时间收集

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
+echo Script stareTiem at %TIME% >> build_log.txt
 @REM "./nsis11/NSIS/makensis.exe"  demo.nsi
 "./NSIS/makensis.exe"  friggaDataCenter.nsi
 timeout /T 2 /NOBREAK
 
 node index.js
 @REM app-builder.exe  blockmap -i .\OutFile\Frigga_Data_Center_1.3.0-9_.exe -o ./OutFile/Frigga_Data_Center_1.3.0-9_.exe.blockmap
+echo Script endTiem at %TIME% >> build_log.txt
